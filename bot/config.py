8756df1b83d6d34704ba08f9a75f8febc64d944a
Yae Miko <aloksinghmains756@gmail.com>
2025-05-31 19:23:28 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,7 +6,7 @@ try:
     BOT_TOKEN = config("BOT_TOKEN", "8191627683:AAErornKfnEpvSlt-l8G0cyexYAR_GOSmeA")
     DEV = 7970350353
     OWNER = config("OWNER", "7970350353")
-    FFMPEG = config("FFMPEG", default='ffmpeg -i "{}" -preset ultrafast -c:v libx265 -crf 30 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',)
+    FFMPEG = config("FFMPEG", 'ffmpeg -i "{}" -vf "scale=854:480" -preset ultrafast -c:v libx265 -crf 30 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"')
     TELEGRAPH_API = config("TELEGRAPH_API", default="https://api.telegra.ph")
     THUMB = config("THUMBNAIL", default="https://graph.org/file/75ee20ec8d8c8bba84f02.jpg")
 except Exception as e:
